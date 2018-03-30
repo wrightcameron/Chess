41108169c7a9fd0fa9350644c52322a8abdd1b7d
Cameron Wright <cameronwright@u.boisestate.edu>
2018-03-30 15:02:48 -0600

    Starting to put most of the board functionality in, though it might make rewriting most of the piecies constructors even though most are not even finished.

--- Board.lua
+++ Board.lua
@@ -5,6 +5,7 @@ local class = require 'middleclass'
 
 Board = class('Board')
 function Board:initialize()
+  --Set the squares of the board.
   self.squares = {}
   for x=1, 8 do
     self.squares[x] = {}
@@ -12,6 +13,18 @@ function Board:initialize()
       self.squares[x][y] = Square:new(x, y)
     end
   end
+  --Define white and black piecies
+  self.whitePieces = {Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),
+                      Rook:new(),Rook:new(),
+                      Knight:new(),Knight(),
+                      Bishop:new(),Bishop(),
+                      Queen:new(),
+                      King:new()}
+  self.blackPieces = {Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),Pawn:new(),
+                      Rook:new(),Rook:new(),
+                      Knight:new(),Knight(),
+                      Bishop:new(),Bishop(),
+                      Queen:new(),
+                      King:new()}
+  --Add pieces to the board.
 end
-
-local board = Board:new() 
