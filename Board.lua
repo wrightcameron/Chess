70ec5880772b359040c6966bf6016d000fe20e09
Cameron Wright <cameronwright@u.boisestate.edu>
2018-03-30 14:28:53 -0600

    Determining if I want to use x & y or a class to make up the grid.

--- Board.lua
+++ Board.lua
@@ -1,13 +1,17 @@
-local Board = {}
-Board.__index = Board
+package.path = package.path .. ";./?.lua"
+local class = require 'Square'
+package.path = package.path .. ";./dependencies/?.lua"
+local class = require 'middleclass'
 
-setmetatable(Board, {
-  __call = function (cls, ...)
-    return cls.new(...)
-  end,
-})
-
-function Board.new(init)
-  local self = setmetatable({}, MyClass)
-  return self
+Board = class('Board')
+function Board:initialize()
+  self.squares = {}
+  for x=1, 8 do
+    self.squares[x] = {}
+    for y=1,8 do
+      self.squares[x][y] = Square:new(x, y)
+    end
+  end
 end
+
+local board = Board:new() 
---
+++ Square.lua
@@ -0,0 +1,31 @@
+package.path = package.path .. ";./dependencies/?.lua"
+local class = require 'middleclass'
+
+Square = class('Square')
+function Square:initialize(x, y)
+  self.x = x
+  self.y = y
+  self.piece = nil
+end
+
+function occupySpot(piece)
+    if self.piece ~= nil then
+            self.piece.setAvailable(false)
+    else 
+        self.piece = piece
+    end
+end
+
+function isOccupied()
+    if self.piece ~= nil then
+        return true
+    else
+        return false
+    end
+end
+
+function releaseSpot()
+    releasedPiece = self.piece
+    self.piece = nil
+    return releasedPiece
+end
