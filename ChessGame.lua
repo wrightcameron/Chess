bf1a0f98f2976fd1eae2901e575818642649dae6
Cameron Wright <cameronwright@u.boisestate.edu>
2018-04-04 11:55:37 -0600

    Started defining the ChessGame class, the driver class.

--- ChessGame.lua
+++ ChessGame.lua
@@ -6,4 +6,31 @@ local class = require 'middleclass'
 --The driver of the chess game, run this.
 print("Ready to play a game of chess?")
 board = Board:new()
+isGameFinised = true
+isWhitePlayer = true
+
 board:printBoard()
+while isGameFinised ~= true do 
+    --Tell players who is going
+    if isWhitePlayer == true then
+        print("Player white's turn.")
+    else
+        print("PLayer black's turn.")
+    end
+    --Open scanner and ask player what they want to do.
+        --Avaible moves
+        --Move piece
+        --Get pieces
+        --print board
+        --help (prints commands.)
+
+
+end
+
+function printBoard(board)
+    board:printBoard()
+end
+
+function isKingCheckMated()
+    return false
+end
