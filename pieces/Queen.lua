616772d99a552c88806aaa23a3df28db3a8a18d6
SpencerJSaunders <36687152+SpencerJSaunders@users.noreply.github.com>
2018-04-04 14:12:22 -0600

    Updated Queen to include validMoves

--- pieces/Queen.lua
+++ pieces/Queen.lua
@@ -9,5 +9,59 @@ function Queen:initialize(type)
 end
 
 function Queen:validMoves(board)
-    return nil
+    
+    moveList = {}
+   
+    local i = 1
+    local j = 1
+
+    for i, 8, 1 do
+        for j, 8, 1 do
+
+            if table.validMove(i, j) then
+                table.insert( moveList, {self.i, self.j})
+            end
+        end
+    end
+
+    i = 1
+    j = 8
+   
+
+    for i, 8, 1 do
+        for j, 1, -1 do
+
+            if table.validMove(i, j) then
+                table.insert( moveList, {self.i, self.j})
+            end
+        end
+    end
+
+    i = 1
+    j = 1
+
+    for j, 8, 1 do
+        for i, 8, 1 do 
+
+            if table.validMove(i, j) then
+                table.insert( moveList, {self.i, self.j})
+            end
+        end
+    end
+
+    i = 8
+    j = 1
+
+    for j, 8, 1 do
+        for i, 1, -1 do
+            if table.validMove(i, j) then
+                table.insert( moveList, {self.i, self.j})
+            end
+        end
+    end
+    
+
+    return moveList
+
+
 end
